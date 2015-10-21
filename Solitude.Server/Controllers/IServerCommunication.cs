44450bef4d6e4d93c18dd63935e162dee58ae0bd
Kasper Steensig <reindeernix@prozum.dk>
2015-10-21 10:53:20 +0200

    server: improved IServerCommunication

--- Solitude.Server/Controllers/IServerCommunication.cs
+++ Solitude.Server/Controllers/IServerCommunication.cs
@@ -3,8 +3,21 @@ using System.Collections.Generic;
 
 namespace Solitude.Server
 {
-	public interface IServerCommunication : IClientCommunication
+	public interface IServerCommunication
 	{
+		// sends offers to the user
+		void RequestMatch(User u);
+
+		//sends n events at max back to the user
+		void GetOwnEvents(int n, bool NEWEST=true);
+
+		void CreateUser (User u);
+		void UpdateUser (User u);
+		void DeleteUser (User u);
+		void UpdateEvent (Event e);
+		void DeleteEvent (Event e);
+		void ReplyOffer (Offer o, bool a);
+		void CancelRegistration (Registration r);
 		void SendMatch (List<Offer> m);
 		void SendOwnEvents (List<Event> e);
 	}
