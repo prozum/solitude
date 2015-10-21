d636ddf3a0b897d2d51a617be76832ead9a02be4
Kasper Steensig <reindeernix@prozum.dk>
2015-10-21 09:29:14 +0200

    [Solitude.Server/MatchingSystem] added matchingsystem

---
+++ Solitude.Server/MatchingSystem/MatchingSystem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Solitude.Server
+{
+	public class MatchingSystem : IMatch
+	{
+		public MatchingSystem ()
+		{
+			
+		}
+			
+		public List<Offer> FindMatch ()
+		{
+			throw new NotImplementedException ();
+		}
+	}
+}
+
