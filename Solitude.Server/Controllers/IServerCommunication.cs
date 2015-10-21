9ea582a34ee22de8b5ec726fd851ce619d251840
Kasper Steensig <reindeernix@prozum.dk>
2015-10-21 10:20:03 +0200

    server: added interface for server communication

---
+++ Solitude.Server/Controllers/IServerCommunication.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Solitude.Server
+{
+	public interface IServerCommunication : IClientCommunication
+	{
+		List<Offer> SendMatch ();
+		List<Event> SendOwnEvents ();
+	}
+}
+
