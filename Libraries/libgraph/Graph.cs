934c38024331a0ffa9a1588365354bd191f34f5b
Kasper Steensig <reindeernix@prozum.dk>
2015-10-22 15:02:31 +0200

    [Libraries/libgraph] removed directed and undirected graphs, replaced them with a single graph class

---
+++ Libraries/libgraph/Graph.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace libgraph
+{
+	public class Graph
+	{
+		public List<Vertex> CategoryVertices = new List<Vertex>();
+
+		public Graph ()
+		{
+			
+		}
+
+		public void AddVertex(Vertex v, Vertex category)
+		{
+
+		}
+
+		public void AddEdge(Vertex v, Vertex w)
+		{
+
+		}
+
+		public void RemoveNode(Vertex v)
+		{
+
+		}
+
+		public void RemoveEdge(Vertex v, Vertex w)
+		{
+
+		}
+
+		public bool TestEdge(Vertex v, Vertex w)
+		{
+
+		}
+	}
+}
+
