2c0960aedfe8176a015ec53c358779b157287fef
Niclas Moeslund Overby <nimoov@prozum.dk>
2015-10-28 10:58:26 +0100

    neo4j: change user vars to properties

--- Samples/Neo4j/Program.cs
+++ Samples/Neo4j/Program.cs
@@ -3,34 +3,37 @@ using Neo4jClient;
 
 namespace Neo4j
 {
+	class User
+	{
+		public int Id { get; set; }
+		public string Name { get; set; }
+	}
+
 	class MainClass
 	{
-		private static GraphClient _graphClient;
-
-		class User
-		{
-			public int Id;
-			public string Name;
-		}
+		private static GraphClient client;
 
 		public static void Main (string[] args)
 		{
-			_graphClient = new GraphClient(new Uri("http://prozum.dk:7474/db/data"),"neo4j","password");
-			_graphClient.Connect ();
+			client = new GraphClient(new Uri("http://prozum.dk:7474/db/data"),"neo4j","password");
+			client.Connect ();
 
 			var newUser = new User { Id = 1, Name = "Test Testesen" };
-			_graphClient.Cypher
+
+			client.Cypher
 				.Create("(user:User {newUser})")
 				.WithParam("newUser", newUser)
 				.ExecuteWithoutResults();
 
-			var res =_graphClient.Cypher
+			var users = client.Cypher
 				.Match ("(user:User)")
 				.Where ((User user) => user.Id == 1)
-				.Return (user => user.Labels ())
+				.Return (user => user.As<User>())
 				.Results;
 
-			Console.Write (res);
+			foreach (var user in users) {
+				Console.WriteLine (user.Name);
+			}
 		}
 	}
 }
