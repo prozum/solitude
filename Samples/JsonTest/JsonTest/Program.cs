026ee00eeba9951ec098be588b1b5987183f1a81
Niclas Moeslund Overby <nimoov@prozum.dk>
2015-10-30 14:50:02 +0100

    jsontest: use restsharp

--- Samples/JsonTest/JsonTest/Program.cs
+++ Samples/JsonTest/JsonTest/Program.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Web;
 using System.Net;
 using System.IO;
+using System.Text;
+using RestSharp;
 
 namespace JsonTest
 {
@@ -11,28 +13,24 @@ namespace JsonTest
 	{
 		public static void Main (string[] args)
 		{
-			string username = "MikkelMus";
-			string password = "hejhej123";
-			string passwordConfirm = "hejhej123";
+			var client = new RestClient("http://127.0.0.1:8080/api/");
+			client.Options (new RestRequest () {RequestFormat = DataFormat.Json,  });
 
-			var jsonDoc = JsonConvert.SerializeObject (new { username = username, password = password, 
-				passwordConfirm = passwordConfirm });
+			var user = new 
+			{	
+				username = "MikkelMus10", 
+				password = "hej%Hej123",
+				passwordConfirm = "hej%Hej123"
+			};
 
-			string url = "http://127.0.0.1:8080/api/user/register";
-			HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create (new Uri (url));
-			request.ContentType = "application/json";
-			request.Method = "POST";
+			var request = new RestRequest("user/register", Method.POST);
+			request.RequestFormat = DataFormat.Json;
+			request.AddBody(user);
+			Console.WriteLine (request.ToString ());
 
-			using (var upStream = new StreamWriter (request.GetRequestStream())) {
-				upStream.Write (jsonDoc);
-				upStream.Flush ();
-				upStream.Close ();
-			}
-//			using (var response = request.GetResponse()) {
-//				string r = response.ToString ();
-//				Console.WriteLine (r);
-//			}
+			IRestResponse response = client.Execute(request);
 
+			Console.WriteLine (response.Content);
 			Console.ReadKey ();
 		}
 	}
