21e7baa0a919269876856c09ec9cbdb5bba33fc3
TobiasMorell <tmorel14@student.aau.dk>
2015-10-30 13:07:22 +0100

    Samples: Truly added the jsontest

---
+++ Samples/JsonTest/JsonTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Web;
+using System.Net;
+using System.IO;
+
+namespace JsonTest
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			string username = "MikkelMus";
+			string password = "hejhej123";
+			string passwordConfirm = "hejhej123";
+
+			var jsonDoc = JsonConvert.SerializeObject (new { username = username, password = password, 
+				passwordConfirm = passwordConfirm });
+
+			string url = "http://prozum.dk:8080/api/user/register";
+			HttpWebRequest request = (HttpWebRequest)HttpWebRequest.Create (new Uri (url));
+			request.ContentType = "application/json";
+			request.Method = "POST";
+
+			using (var upStream = new StreamWriter (request.GetRequestStream())) {
+				upStream.Write (jsonDoc);
+				upStream.Flush ();
+				upStream.Close ();
+			}
+//			using (var response = request.GetResponse()) {
+//				string r = response.ToString ();
+//				Console.WriteLine (r);
+//			}
+
+			Console.ReadKey ();
+		}
+	}
+}
