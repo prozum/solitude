ad3d568b069955810427b7af919ff3509d4832a2
reindeernix <reindeernix@prozum.dk>
2015-10-30 19:41:53 +0100

    will add matchingsystem later, working on DAL now

---
+++ Libraries/DAL/DAL.cs
@@ -0,0 +1,23 @@
+﻿using System;
+<<<<<<< HEAD
+using Neo4jClient;
+using Neo4jClient.Cypher;
+
+namespace DAL
+{
+	public static class DAL
+	{
+		
+=======
+
+namespace DAL
+{
+	public class MyClass
+	{
+		public MyClass ()
+		{
+		}
+>>>>>>> 2ce265807f2c1dbbe500febcc2af7c407d61f5c5
+	}
+}
+
---
+++ Libraries/DAL/Models/Event.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace DAL
+{
+	public class Event
+	{
+		public int Id { get; set; }
+		public string Date { get; set; }
+		public string Address { get; set; }
+		public string Description { get; set; }
+	}
+}
+
