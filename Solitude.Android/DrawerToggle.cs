7cf203283788672fa737faacd0136bc66b407079
Lee Paludan <Leepaludan@gmail.com>
2015-11-02 09:45:06 +0100

    Added Drawer and reorganized folders
    Added the drawer stuff that I worked on and changed a few things. Could
    not test it due to build errors due to the communication API

---
+++ Solitude.Android/DrawerToggle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Android.Support.V4.App;
+using Android.App;
+using Android.Support.V4.Widget;
+using Android.Views;
+
+namespace DineWithaDane.Android
+{
+	public class DrawerToggle : ActionBarDrawerToggle
+	{
+		AbstractActivity _currentActivity;
+
+		public DrawerToggle (AbstractActivity currentActivity, DrawerLayout layout, int imageResource, int openDrawerDesc, int closeDrawerDesc) 
+			: base (currentActivity, layout, imageResource, openDrawerDesc, closeDrawerDesc)
+		{
+			_currentActivity = currentActivity;
+		}
+
+		public override void OnDrawerOpened (Android.Views.View drawerView)
+		{
+			base.OnDrawerOpened (drawerView);
+		}
+
+		public override void OnDrawerClosed (Android.Views.View drawerView)
+		{
+			base.OnDrawerClosed (drawerView);
+		}
+
+		public override void OnDrawerSlide (Android.Views.View drawerView, float slideOffset)
+		{
+			base.OnDrawerSlide (drawerView, slideOffset);
+		}
+	}
+}
+
