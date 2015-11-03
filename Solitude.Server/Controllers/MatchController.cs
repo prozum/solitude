b8d5cab64256144eb6b57c22bbcb995462bc6aab
Esben Kirkegaard <esben411@hotmail.com>
2015-11-03 09:11:13 +0100

    server: matchcontroller update

--- Solitude.Server/Controllers/MatchController.cs
+++ Solitude.Server/Controllers/MatchController.cs
@@ -1,13 +1,22 @@
 ﻿using System;
 using System.Web.Http;
+using Newtonsoft.Json;
+using DAL;
+using System.Collections.Generic;
 
 namespace Solitude.Server
 {
+    [RoutePrefix("api/match")]
     public class MatchController : ApiController
     {
-        public MatchController()
+        int uid;
+        public MatchController(int uid)
         {
+            this.uid = uid;
+        }
+        public List<Event> Get(int limit)
+        {
+            return DAL.DAL.MatchUser(uid, limit);
         }
     }
 }
-
