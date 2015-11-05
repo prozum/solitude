b4f18a58e129114ab8c7ff58a53fd3862d543ecf
Jimmi Holst Christensen <rainbowhejsil@gmail.com>
2015-11-05 12:52:36 +0100

    solitude.android: added maxslots and slots left to event group

---
+++ Solitude.Android/TileMenu/InfoTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace DineWithaDane.Android
+{
+	public class InfoTest
+	{
+		public string Name
+		{
+			get;
+			set;
+		}
+
+		public string Description
+		{
+			get;
+			set;
+		}
+
+		public InfoTest(string name, string desc)
+		{
+			Name = name;
+			Description = desc;
+		}
+	}
+}
+
--- Solitude.Android/TileMenu/Items/EventGroup.cs
+++ Solitude.Android/TileMenu/Items/EventGroup.cs
@@ -19,10 +19,18 @@ namespace DineWithaDane.Android
 		public string Title { set { TitleView.Text = "Title: " + value; } }
 		public string Place	{ set { PlaceView.Text = "Place: " + value;	} }
 		public DateTime Date { set { DateView.Text = "Date: " + value; } }
+		public Tuple<int,int> Slots 
+		{
+			set 
+			{ 
+				ParticipantsView.Text = "Participants: " + (value.Item2 - value.Item1) + "/" + value.Item2; 
+			} 
+		}
 
 		protected TextView TitleView { get; set; }
 		protected TextView PlaceView { get; set; }
 		protected TextView DateView { get; set; }
+		protected TextView ParticipantsView { get; set; }
 		#endregion
 
 
@@ -33,6 +41,7 @@ namespace DineWithaDane.Android
 			TitleView = new TextView(context);
 			PlaceView = new TextView(context);
 			DateView = new TextView(context);
+			ParticipantsView = new TextView(context);
 
 			Initialize();
 		}
@@ -47,11 +56,13 @@ namespace DineWithaDane.Android
 
 			AddView(TitleView);
 			AddView(detailslayout);
+			AddView(ParticipantsView);
 			detailslayout.AddView(PlaceView);
 			detailslayout.AddView(DateView);
 
 			detailslayout.WeightSum = 2;
 			TitleView.LayoutParameters.Width = -1;
+			ParticipantsView.LayoutParameters.Width = -1;
 			PlaceView.LayoutParameters = new LinearLayout.LayoutParams(0, -2, 1);
 			DateView.LayoutParameters = new LinearLayout.LayoutParams(0, -2, 1);
 
