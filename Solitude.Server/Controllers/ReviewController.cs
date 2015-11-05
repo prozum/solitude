d6b28c6bed1ff131ac4e3a36cb9ef4e66eb7e649
Niclas Moeslund Overby <nimoov@prozum.dk>
2015-11-05 13:22:08 +0100

    server: add review support

--- Solitude.Server/Controllers/ReviewController.cs
+++ Solitude.Server/Controllers/ReviewController.cs
@@ -1,11 +1,15 @@
 ﻿using System.Web.Http;
+using System.Threading.Tasks;
 
 namespace Solitude.Server
 {
+    [RoutePrefix("api/review")]
     public class ReviewController : ApiController
     {
-        public ReviewController()
+        [Route("add")]
+        public async Task<IHttpActionResult> Add(ReviewModel model)
         {
+            return Ok(model);
         }
     }
 }
