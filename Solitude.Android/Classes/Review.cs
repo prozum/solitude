143610a460a1310a57c623f9916699f8e6baa77c
Lee Paludan <Leepaludan@gmail.com>
2015-11-05 13:19:54 +0100

    Solitude.Android : Review
    Added communication between the user review and the server. This
    includes the receiving (from textfield) and sending (to the server).

--- Solitude.Android/Classes/Review.cs
+++ Solitude.Android/Classes/Review.cs
@@ -2,17 +2,72 @@ using System;
 using System.Collections.Generic;
 using DineWithaDane.Android;
 using System.Threading.Tasks;
+using Android.App;
+using Android.Widget;
+using Android.Views;
+using ClientCommunication;
 
 namespace DineWithaDane.Android
 {
 	public class Review
 	{		
 		User _user;
-		List<Review> userReviews;
+		Activity _currentActivity;
+		RatingBar rating;
+		string newReview;
 
-		public Review (User user)
+		public string ReviewText 
+		{
+			get {
+				newReview;
+			}
+		}
+
+		public int Rating
+		{
+			get {
+				rating.NumStars;	
+			}
+		}
+
+		public Review (User user, Activity currentActivity)
 		{
 			_user = user;
+			_currentActivity = currentActivity;
+
+			setupReviewDialog ();
+		}
+
+		private void setupReviewDialog ()
+		{
+			var dialog = new Dialog(_currentActivity);
+			Button cancelButton, acceptButton;
+			EditText input;
+
+			dialog.RequestWindowFeature((int) WindowFeatures.NoTitle);
+			dialog.SetContentView(Resource.Layout.review_layout);
+
+			rating = (RatingBar)dialog.FindViewById (Resource.Id.ratingbar);
+			dialog.Show();
+
+			cancelButton = (Button) dialog.FindViewById <Button> (Resource.Id.cancelReviewButton);
+			acceptButton = (Button) dialog.FindViewById <Button> (Resource.Id.postReviewButton);
+			input = (EditText)dialog.FindViewById <EditText> (Resource.Id.reviewUserInput);
+
+			cancelButton.Click += (object sender, EventArgs e) => 
+			{
+				dialog.Dismiss();
+			};
+
+			acceptButton.Click += (object sender, EventArgs e) => 
+			{
+				newReview = input.Text;
+			};
+		}
+
+		private void sendReview ()
+		{
+			CommunicationInterface.PostReview (this);
 		}
 	}
 }
