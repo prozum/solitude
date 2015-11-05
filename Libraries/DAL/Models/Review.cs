fccfcf54d2cf7c6e0eb1f0c580c5c124ea8b7b7f
Niclas Moeslund Overby <nimoov@prozum.dk>
2015-11-05 14:11:53 +0100

    server: add reviews to db

---
+++ Libraries/DAL/Models/Review.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Dal
+{
+	public class Review
+	{
+		public string ReviewText { set; get; }
+		public int Rating { set; get; }
+	}
+}
+
--- Solitude.Server/Controllers/ReviewController.cs
+++ Solitude.Server/Controllers/ReviewController.cs
@@ -1,15 +1,29 @@
 ﻿using System.Web.Http;
+using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Dal;
 
 namespace Solitude.Server
 {
     [RoutePrefix("api/review")]
     public class ReviewController : ApiController
     {
+        public DatabaseAbstrationLayer DB
+        {
+            get
+            {
+                return Request.GetOwinContext().Get<DatabaseAbstrationLayer>();
+            }
+        }
+
+        [Authorize]
         [Route("add")]
         public async Task<IHttpActionResult> Add(ReviewModel model)
         {
-            return Ok(model);
+            await DB.AddReview(User.Identity.GetUserId(), model.ReviewText, model.Rating);
+            return Ok();
         }
     }
 }
