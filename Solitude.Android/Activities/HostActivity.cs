ccb2f1d7efa5eb6b30dc625b12e4653c15316ba1
Frederik Palmelund Voldby <frederik.palmelund@gmail.com>
2015-11-06 16:02:33 +0100

    android/host: work on hosting event

--- Solitude.Android/Activities/HostActivity.cs
+++ Solitude.Android/Activities/HostActivity.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Collections.ObjectModel;
 
 using Android.App;
 using Android.Content;
@@ -9,17 +10,50 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Android.Util;
 
 namespace DineWithaDane.Android
 {
 	[Activity (Label = "Hosts")]			
 	public class HostActivity : AbstractActivity
 	{
+		ObservableCollection<Event> hostedEventsList;
+
 		protected override void OnCreate (Bundle bundle)
 		{
 			// setting up and drawer
 			drawerPosition = 3;
 			base.OnCreate (bundle);
+
+			hostedEventsList = new ObservableCollection<Event>();
+
+			var content = FindViewById<FrameLayout>(Resource.Id.content_frame);
+			var internalContent = new LinearLayout(this);
+			internalContent.Orientation = Orientation.Vertical;
+			content.AddView(internalContent);
+
+			var metrics = new DisplayMetrics();
+			WindowManager.DefaultDisplay.GetMetrics(metrics);
+
+			var hostNewEventButton = new Button(this);
+			hostNewEventButton.Text = "Host New Event";
+
+			hostNewEventButton.Click += (object sender, EventArgs e) => StartActivity(typeof(CreateEventActivity));
+
+			hostedEventsList.CollectionChanged += (object sender, System.Collections.Specialized.NotifyCollectionChangedEventArgs e) => 
+			{
+				foreach (var item in hostedEventsList)
+				{
+					// Add element with event
+				}
+				content.AddView(hostNewEventButton);
+			};
+
+			foreach (var item in hostedEventsList)
+			{
+				// Add element with event
+			}
+			internalContent.AddView(hostNewEventButton);
 		}
 	}
 }
