f80606363e21b376377962066f8c8e95c1297c52
Esben Kirkegaard <esben411@hotmail.com>
2015-11-10 15:02:53 +0100

    Solitude.Server.Test : Updated tests

--- Solitude.Server.Tests/EventControllerTest.cs
+++ Solitude.Server.Tests/EventControllerTest.cs
@@ -6,6 +6,8 @@ using Solitude.Server;
 using Model;
 using System.Net;
 using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Solitude.Server.Tests
 {
@@ -15,7 +17,7 @@ namespace Solitude.Server.Tests
 		private int eventID;
 		Event e = new Event ();
 
-		[SetUp()]
+		[TestFixtureSetUp()]
 		public void StartUp()
 		{
 			e.Address = "Test Street 101";
@@ -24,16 +26,17 @@ namespace Solitude.Server.Tests
 			e.SlotsTaken = 0;
 			e.SlotsTotal = 100;
 			e.Title = "[Test]" + Values.r.Next (0, 9999);
-			e.UserId = "0";
 		}
 
 		[Test()]
 		public void TestAddEvent()
 		{
-			var request = buildRequest ("event/add", Method.POST);
+			var request = buildRequest ("event", Method.POST);
+			request.AddBody (e);
 
 			var response = Values.testClient.Execute (request);
-
+			dynamic jVal = JsonConvert.DeserializeObject(response.Content);
+			e.Id = jVal.Id;
 			Assert.AreEqual (HttpStatusCode.OK, response.StatusCode, "The request was not executed correctly: " + response.Content);
 
 			dynamic dynObj = JsonConvert.DeserializeObject (response.Content);
@@ -43,50 +46,52 @@ namespace Solitude.Server.Tests
 		[Test()]
 		public void TestGetEvent()
 		{
-			var request = buildRequest (string.Format ("event/{0}", e.Id), Method.GET);
+			var request = buildRequest (string.Format ("event"), Method.GET);
 
 			var response = Values.testClient.Execute (request);
 
 			Assert.AreEqual (HttpStatusCode.OK, response.StatusCode, "The request was not executed correctly: " + response.Content);
 
-			Event receivedEvent = JsonConvert.DeserializeObject<Event> (response.Content);
+			Event receivedEvent = parseEvents (response);
 
-			Assert.AreEqual (e, receivedEvent, "The recieved event was not equal to the one created");
+			Assert.AreEqual (e.Id, receivedEvent.Id, "The received event was not equal to the one created");
 		}
 
 		[Test()]
 		public void TestUpdateEventChangeTitle()
 		{
 			e.Title = "[Modified Test Event]";
-			var request = buildRequest ("event/update", Method.PUT);
+			var request = buildRequest ("event", Method.PUT);
+
+			request.AddBody (e);
 
 			var response = Values.testClient.Execute (request);
 
-			Assert.AreEqual (HttpStatusCode.OK, response.StatusCode, "The request was not executed corrextly: " + response.Content);
+			Assert.AreEqual (HttpStatusCode.OK, response.StatusCode, "The request was not executed correctly: " + response.Content);
 
 			//Now get the event from the server again:
-			request = buildRequest (string.Format ("event/{0}", e.Id), Method.GET);
+			request = buildRequest (string.Format ("event"), Method.GET);
 
 			response = Values.testClient.Execute (request);
 
 			Assert.AreEqual (HttpStatusCode.OK, response.StatusCode, "The get-request was not executed correctly: " + response.Content);
 
-			Event receivedEvent = JsonConvert.DeserializeObject<Event> (response.Content);
+			Event receivedEvent = parseEvents (response);
 
 			Assert.AreEqual (e.Title, receivedEvent.Title, "The recieved event did not have the updated title.");
-		}
+		} 
 
 		[Test()]
 		public void zzTestDeleteEvent()
 		{
-			var request = buildRequest ("event/delete", Method.DELETE);
+			var request = buildRequest ("event/" + e.Id, Method.DELETE);
 
 			var response = Values.testClient.Execute (request);
 
 			Assert.AreEqual (HttpStatusCode.OK, response.StatusCode, "The request was not executed correctly: " + response.Content);
 
 			//Now try to get event:
-			request = buildRequest (string.Format ("event/{0}", e.Id), Method.GET);
+			request = buildRequest (string.Format ("event", e.Id), Method.GET);
 
 			response = Values.testClient.Execute (request);
 
@@ -101,6 +106,48 @@ namespace Solitude.Server.Tests
 			request.RequestFormat = DataFormat.Json;
 
 			return request;
+		}
+
+		private Event parseEvents(IRestResponse serverResponse){
+			//Tries to convert response to events
+			var events = new List<Event>();
+
+			//Extract every single json to it's own JsonValue
+			Regex reg = new Regex(@"{[^}]*}");
+			var matches = reg.Matches(serverResponse.Content);
+
+			//Generate Events from the JsonValues
+			for(int i = 0; i < matches.Count; i++)
+			{
+				//Generate Events from the JsonValues
+				try {
+					dynamic jVal = JsonConvert.DeserializeObject(matches[i].Value);
+					int ID = jVal.Id;
+					string title = jVal.Title;
+					string desc = jVal.Description;
+					string dateTime = jVal.Date;
+					string adress = jVal.Address;
+					int slotsTotal = jVal.SlotsTotal;
+					int slotsTaken = jVal.SlotsTaken;
+
+					Event  eNew = new Event();
+					eNew.Address =  adress;
+					eNew.Date = dateTime;
+					eNew.Description = desc;
+					eNew.Id = ID;
+					eNew.SlotsTaken = slotsTaken;
+					eNew.SlotsTotal = slotsTotal;
+					eNew.Title = title;
+
+					events.Add(eNew);
+				}
+				catch
+				{
+				}
+			}
+
+			Event eRight = events.Find ((evnt) => evnt.Id == e.Id);
+			return eRight;
 		}
 	}
 }
---
+++ Solitude.Server.Tests/zDeleteTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using RestSharp;
+using NUnit.Framework;
+using System.Net;
+
+namespace Solitude.Server.Tests
+{
+	[TestFixture ()]
+	public class zDeleteTest
+	{
+		[Test ()]
+		public void DeleteTest ()
+		{
+			var deleteRequest = new RestRequest ("user", Method.DELETE);
+			deleteRequest.RequestFormat = DataFormat.Json;
+
+			deleteRequest.AddHeader("Authorization", "bearer " + Values.testToken);
+
+			//Adds body to the request
+			var body = new {
+				userToken = Values.testToken
+			};
+			deleteRequest.AddBody(body);
+			var response = Values.testClient.Execute (deleteRequest);
+
+			var request = new RestRequest("token", Method.POST);
+
+			request.AddHeader("content-type", "x-www-form-urlencoded");
+			request.AddHeader ("postman-token", "a4e85886-daf2-5856-b530-12ed21af5867");
+			request.AddHeader("cache-control", "no_cache");
+
+			request.AddParameter("x-www-form-urlencoded", String.Format("username={0}&password={1}&grant_type=password", Values.testUsername, Values.password), ParameterType.RequestBody);
+
+			var tokenResponse = Values.testClient.Execute (request);
+
+			Assert.IsFalse (tokenResponse.StatusCode == HttpStatusCode.OK, "Login succeeded unexpectedly " + response.StatusCode.ToString());
+		} 
+	}
+}
+
