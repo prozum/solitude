eedbbaf3fdce5050cf55691841a617488b3fe66e
Tobias Morell <tmorel14@student.aau.dk>
2015-11-10 09:24:28 +0100

    Solitude.Server.Test : Added login method

--- Solitude.Server.Tests/Test.cs
+++ Solitude.Server.Tests/Test.cs
@@ -2,6 +2,7 @@
 using System;
 using RestSharp;
 using System.Net;
+using Newtonsoft.Json;
 
 namespace Solitude.Server.Tests
 {
@@ -10,7 +11,8 @@ namespace Solitude.Server.Tests
 	{
 		RestClient testClient = new RestClient("http://prozum.dk:8080/api/");
 		Random r = new Random();
-		string testUsername = "ServerTestUser" + r.Next(1, 100000) + r.Next(1, 100000) + r.Next(1, 100000);
+		string testUsername = "TestKurt" + r.Next(1, 100000), testToken = "", token_type = "";
+
 		[Test ()]
 		public void TestCase ()
 		{
@@ -34,6 +36,35 @@ namespace Solitude.Server.Tests
 			request.AddBody (user);
 			var response = testClient.Execute (request);
 			Assert.IsTrue (response.ResponseStatus == HttpStatusCode.OK);
+		}
+
+		public void Login()
+		{
+			//Generates the token-request
+			var request = new RestRequest("token", Method.POST);
+
+			//Adds headers to notify server that it's a token-request
+			request.AddHeader("content-type", "x-www-form-urlencoded");
+			request.AddHeader ("postman-token", "a4e85886-daf2-5856-b530-12ed21af5867");
+			request.AddHeader("cache-control", "no_cache");
+
+			//Adds body including username and password and specify, that a grant_type as password is desired
+			request.AddParameter("x-www-form-urlencoded", String.Format("username={0}&password={1}&grant_type=password", testUsername, "Testkurt123!"), ParameterType.RequestBody);
+
+			//Execute and await response, parse afterwards
+			var tokenResponse = testClient.Execute (request);
+
+			Assert.IsTrue (tokenResponse.StatusCode != 0, "[Login failed] Likely there's no internet connection.");
+
+			dynamic dynObj = JsonConvert.DeserializeObject (tokenResponse);
+
+			Assert.IsTrue (tokenResponse.StatusCode == HttpStatusCode.OK);
+
+			testToken = dynObj.access_token;
+			token_type = dynObj.token_type;
+
+			Assert.AreNotEqual (testToken, "");
+			Assert.AreNotEqual (token_type, "");
 		}
 	}
 }
