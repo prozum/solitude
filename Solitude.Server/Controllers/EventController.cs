7cca47506f10cd891cdfd07185d40bffaf805c85
Niclas Moeslund Overby <nimoov@prozum.dk>
2015-11-11 11:04:52 +0100

    server: rename eventcontroller to hostcontroller server: add new eventcontroller

--- Solitude.Server/Controllers/EventController.cs
+++ Solitude.Server/Controllers/EventController.cs
@@ -1,40 +1,22 @@
 ﻿using System.Web.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
-using System.Collections.Generic;
-using Model;
 
 namespace Solitude.Server
 {
     public class EventController : SolitudeController
-	{
+    {
         public async Task<IHttpActionResult> Get()
         {
-            var events = await DB.GetHostingEvents(User.Identity.GetUserId());
+            var events = await DB.GetAttendingEvents(User.Identity.GetUserId());
             return Ok(events);
-		}
-            
-        public async Task<IHttpActionResult> Post(Event e)
-        {
-            e.UserId = User.Identity.GetUserId();
-
-            await DB.AddEvent(e);
-
-            return Ok(new { Id = e.Id});
         }
-            
+
         public async Task<IHttpActionResult> Delete(int id)
         {
-            await DB.DeleteEvent(id);
-
+            await DB.CancelRegistration(User.Identity.GetUserId(), id);
             return Ok();
         }
-            
-        public async Task<IHttpActionResult> Put(Event e)
-        {
-            await DB.UpdateEvent(e);
-
-            return Ok();
-        }
-	}
+    }
 }
+
