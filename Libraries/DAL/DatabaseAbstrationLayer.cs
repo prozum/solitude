91ac3434eecef6c26885fd25878ccb564401fee7
Kasper Steensig <reindeernix@prozum.dk>
2015-11-12 10:02:12 +0100

    server: fixed comparing

--- Libraries/DAL/DatabaseAbstrationLayer.cs
+++ Libraries/DAL/DatabaseAbstrationLayer.cs
@@ -218,7 +218,7 @@ namespace Dal
 				//make sure that the interest is related with the right user
 				.Match ("(user:User), (language:Language)")
 				.Where((User user) => user.Id == uid)
-				.AndWhere ("language.Id == {lc}")
+				.AndWhere ("language.Id = {lc}")
 				.WithParam ("lc", lc)
 				//create a unique relation "WANTS" with the weight 'w'
 				.CreateUnique (("user-[:WANTS {weight}]->language"))
@@ -273,7 +273,7 @@ namespace Dal
 				//make sure that the interest is related with the right user
 				.Match ("(user:User), (foodhabit:FoodHabit)")
 				.Where((User user) => user.Id == uid)
-				.AndWhere ("foodhabit.Id == {fh}")
+				.AndWhere ("foodhabit.Id = {fh}")
 				.WithParam ("fh", fh)
 				//create a unique relation "WANTS" with the weight 'w'
 				.CreateUnique (("user-[:WANTS {weight}]->foodhabit"))
